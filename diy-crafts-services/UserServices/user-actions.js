f08e366fbe8ce008756a98fec26da1afb64a4d84
MuthuNithya <nithyarad@gmail.com>
2016-10-13 23:14:03 -0700

    contact target updated

--- diy-crafts-services/UserServices/user-actions.js
+++ diy-crafts-services/UserServices/user-actions.js
@@ -10,7 +10,7 @@
     var matchedEmail;
     var userActions = {
         suggestContact: function(req, res){
-            if(req &&req.headers['x-diycrafts-target']&& (req.headers['x-diycrafts-target'] === 'DIYCRAFTS_SUGGEST'|| req.headers['x-diycrafts-contact'] === 'DIYCRAFTS_CONTACTUS')){
+            if(req &&req.headers['x-diycrafts-target']&& (req.headers['x-diycrafts-target'] === 'DIYCRAFTS_SUGGEST'|| req.headers['x-diycrafts-target'] === 'DIYCRAFTS_CONTACTUS')){
                 if(req.body && req.body.email && req.body.name && req.body.subject && req.body.comment){
                     mailServices.sendMail(req.body.email, 'letscreate@diycraftsnme.com', req.body.subject, req.body.comment, req.body.name, res);
                 }else{

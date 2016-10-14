69a35a811c600fa6aecdd59a2fb362fbb6a59460
diycraftsnme <diycraftsnme@gmail.com>
2016-10-13 19:54:22 -0700

    Update server.js

--- diy-crafts-services/server.js
+++ diy-crafts-services/server.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 });
 
 // Start the server
-app.set('port', 8000);
+app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
